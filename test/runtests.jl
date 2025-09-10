f822c2ccf49ddb41a6b724514c2fca3de6345d58
tortar <adrianomeligrana1999@gmail.com>
2025-09-10 23:59:25 +0200

    Fix aqua tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,8 +7,8 @@ using Aqua, Test
 
     if "CI" in keys(ENV)
         @testset "Code quality (Aqua.jl)" begin
-            Aqua.test_all(WeightVectors, deps_compat=false)
-            Aqua.test_deps_compat(WeightVectors, check_extras=false)
+            Aqua.test_all(WrappedUnions, deps_compat=false)
+            Aqua.test_deps_compat(WrappedUnions, check_extras=false)
         end
     end
 end
